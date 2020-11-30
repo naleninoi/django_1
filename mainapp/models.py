3ca11d7986965f1dcc66f194dc2e93568835cc30
Viktor Kochetov <naleninoi@gmail.com>
2020-11-30 13:17:58 +0300

    lesson 3

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=128, verbose_name='Название')
+    image = models.ImageField(upload_to='products_images', blank=True, verbose_name='Изображение')
+    short_desc = models.CharField(max_length=120, verbose_name='Краткое описание')
+    description = models.TextField(verbose_name='Описание', blank=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество на складе')
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,23 +1,25 @@
+import os, datetime, json
+
 from django.shortcuts import render
+from django.conf import settings
 
 
 # Create your views here.
+from mainapp.models import Product, ProductCategory
+
 
 def main(request):
+    title = 'Главная'
+    products = Product.objects.all()[:3]
     content = {
-        'title': 'Главная'
+        'title': title,
+        'products': products
     }
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'}
-    ]
+def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
     content = {
         'title': 'Продукты',
         'links_menu': links_menu
@@ -26,9 +28,12 @@ def products(request):
 
 
 def contact(request):
-    content = {
-        'title': 'Контакты'
-    }
+    title = 'Контакты'
+    visit_date = datetime.datetime.now()
+    file_path = os.path.join(settings.BASE_DIR, 'json/contacts.json')
+    with open(file_path, encoding='utf-8') as file_contacts:
+        locations = json.load(file_contacts)
+    content = {'title': title, 'visit_date': visit_date, 'locations': locations}
     return render(request, 'mainapp/contact.html', content)
 
 

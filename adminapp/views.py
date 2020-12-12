bf2a9ad27fea31c179490a8502e1789ac27833be
Viktor <naleninoi@gmail.com>
2020-12-12 12:30:47 +0300

    Add_7-3

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,11 +2,14 @@ from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
 
+
+# USERS
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
@@ -20,7 +23,7 @@ def users(request):
 
 def user_create(request):
     if request.method == 'POST':
-        user_form = ShopUserRegisterForm(request.POST)
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
         if user_form.is_valid():
             user_form.save()
             return HttpResponseRedirect(reverse('admin:users'))
@@ -34,17 +37,39 @@ def user_create(request):
 
 
 def user_update(request, pk):
-    pass
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {
+        'update_form': edit_form
+    }
+    return render(request, 'adminapp/user_update.html', content)
 
 
 def user_delete(request, pk):
-    pass
+    user_item = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        if user_item.is_active:
+            user_item.is_active = False
+        else:
+            user_item.is_active = True
+        user_item.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+    content = {'user_to_delete': user_item}
+    return render(request, 'adminapp/user_delete.html', content)
 
 
+# CATEGORIES
 def categories(request):
     title = 'админка/категории'
 
-    categories_list = ProductCategory.objects.all()
+    categories_list = ProductCategory.objects.all().order_by('-is_active', 'name')
 
     content = {
         'title': title,
@@ -55,26 +80,59 @@ def categories(request):
 
 
 def category_create(request):
-    pass
+    if request.method == 'POST':
+        category_form = ProductCategoryEditForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect(reverse('admin:categories'))
+    else:
+        category_form = ProductCategoryEditForm()
+
+    content = {
+        'update_form': category_form
+    }
+    return render(request, 'adminapp/category_update.html', content)
 
 
 def category_update(request, pk):
-    pass
+    edit_category = get_object_or_404(ProductCategory, pk=pk)
+    if request.method == 'POST':
+        edit_form = ProductCategoryEditForm(request.POST, instance=edit_category)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:category_update', args=[edit_category.pk]))
+    else:
+        edit_form = ProductCategoryEditForm(instance=edit_category)
+
+    content = {
+        'update_form': edit_form
+    }
+    return render(request, 'adminapp/category_update.html', content)
 
 
 def category_delete(request, pk):
-    pass
+    category_item = get_object_or_404(ProductCategory, pk=pk)
+    if request.method == 'POST':
+        if category_item.is_active:
+            category_item.is_active = False
+        else:
+            category_item.is_active = True
+        category_item.save()
+        return HttpResponseRedirect(reverse('admin:categories'))
+    content = {'category_to_delete': category_item}
+    return render(request, 'adminapp/category_delete.html', content)
 
 
+# PRODUCTS
 def products(request, pk):
     title = 'админка/продукт'
 
-    category = get_object_or_404(ProductCategory, pk=pk)
+    category_item = get_object_or_404(ProductCategory, pk=pk)
     products_list = Product.objects.filter(category__pk=pk).order_by('name')
 
     content = {
         'title': title,
-        'category': category,
+        'category': category_item,
         'objects': products_list,
     }
 
